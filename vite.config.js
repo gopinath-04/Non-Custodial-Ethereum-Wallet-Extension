a75265c4d98cf08f54cf7767c08b038bf335179a
gopinath-04 <gopinathpanigrahi2004@gmail.com>
2026-01-22 23:17:09 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'manifest.json',
+          dest: '.'
+        }
+      ]
+    })
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'src/background/background.js'),
+      },
+      output: {
+        entryFileNames: 'src/[name]/[name].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+  },
+});
